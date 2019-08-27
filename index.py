9d496adca5d390ff8ba4b3ce539ee18ce5d59a4d
hugojose14 <hugo.jose.1@hotmail.com>
2019-08-27 16:06:44 -0500

    seven commit

--- index.py
+++ index.py
@@ -46,5 +46,5 @@ def page_not_found(error):
 	return render_template("error.html",error="Página no encontrada..."), 404
 
 if __name__ == '__main__':
-    app.run(port=3000,debug=True)
+    app.run(host='0.0.0.0',port=80,debug=True)
 
